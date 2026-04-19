3face6a66664c597720c0fd210095ef1d2072097
sunruntian <sun_run_tian@qq.com>
2026-04-19 20:44:25 +0800

    费马小定理

---
+++ Inv.cpp
@@ -0,0 +1,23 @@
+/*
+
+
+ll power(ll a,ll b,ll c){
+    ll ret=1;
+    a%=c; 
+    while(b){
+        if(b&1){ 
+            ret=(ret*a)%c;
+        }
+        a=(a*a)%c;
+        b>>=1;
+    }
+    return ret;
+}
+ll inv(ll a,ll mod){
+    return power(a,mod-2,mod);
+}
+
+
+
+
+*/
